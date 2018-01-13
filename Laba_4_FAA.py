a1fe22de2bd83e4afd483eefcb8e8659b16667a3
Anastasia <33443058+AnastsiaFAA@users.noreply.github.com>
2018-01-14 01:56:27 +0700

    Update Laba_4_FAA.py

--- Laba_4_FAA.py
+++ Laba_4_FAA.py
@@ -1,3 +1,8 @@
+#Используя Python  и библиотеки Queue и Thread, а так же код из 3 задания, 
+#написать скрипт, который создает фоновый поток и в нем периодично обновляет страницу 
+#новостного агенства и отслеживает новые новости(которые ещё не выводились). 
+#Фоновый поток использует объект очередь для передачи в основной поток новый новостей. 
+#Новости выводятся на печать из основного потока.
 import requests
 from bs4 import BeautifulSoup
 from queue import Queue, Empty
@@ -10,37 +15,36 @@ import datetime
 def get_posts(url, queue):
     while True:
 
-        PageHTML = requests.get(url).text
-        Soup = BeautifulSoup(PageHTML, "html.parser")
-        posts = []
+        PageHTML = requests.get(url).text          #получить инфу со страницы в виде текста
+        Soup = BeautifulSoup(PageHTML, "html.parser") #представить как вложенную структуру данных
+        posts = []      #создается список "посты"
 
         for i in Soup.find_all("div", "story__main"):
-            # print("\n\t\t\t***\n")
-            # print(i)
+                      #ищет в html-коде класс "story_main"             
             tagos = []
             title = []
             author = []
             for titles in i.find_all("a", "story__title-link "):
                 title = titles.text
-            for tags in i.find_all("a", "story__tag"):
-                tag = tags.get_text()
-                tagos.append(tag[9:-8])
+            for tags in i.find_all("a", "story__tag"): #ищет теги
+                tag = tags.get_text()                  #изклекает текст из класса теги
+                tagos.append(tag[9:-8])                #добавляет в список, обрезая лишнее
             for authors in i.find_all("a", "story__author"):
                 author = authors.get_text()
-
+            #в список "посты" для каждого класса "story_main" добавляется словарь с такими ключами:
             posts.append({'Title:': title, "Tags:": tagos, "Author:": author})
-
-            try:
+            #обработка исключения
+            try:                        #предложение-инструкция
                 if title not in all_titles:
                     all_titles.add(title)
 
                     queue.put({'Author:': author,
                                'Title:': title,
                                'Tags:': tagos})
-            except TypeError:
-                pass
+            except TypeError:   #операция применена к объекту несоответствующего типа
+                pass       #остановка
 
-        time.sleep(10)  
+        time.sleep(10)  #каждые 10сек обновляет новости
 
 
 if __name__ == "__main__":
@@ -58,5 +62,5 @@ if __name__ == "__main__":
             while not queue.empty():
                 print(queue.get())
                 print(datetime.datetime.now())
-        time.sleep(0)
+        time.sleep(0)  
         
